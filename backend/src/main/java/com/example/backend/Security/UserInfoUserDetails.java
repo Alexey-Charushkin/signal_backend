49dced1260a428ce015d313d1c66159ceb14e2c0
SergeySushko1990 <120659163+SergeySushko1990@users.noreply.github.com>
2023-06-26 22:22:53 +0300

    Spring Security

---
+++ backend/src/main/java/com/example/backend/Security/UserInfoUserDetails.java
@@ -0,0 +1,61 @@
+package com.example.backend.Security;
+
+import com.example.backend.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserInfoUserDetails implements UserDetails {
+
+    private String username;
+    private String password;
+    private List<GrantedAuthority> authorities;
+
+    public UserInfoUserDetails(User user){
+        username = user.getEmail();
+        password = user.getPassword();
+        authorities = Arrays.stream(user.getRole().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ backend/src/main/java/com/example/backend/Security/UserInfoUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.backend.Security;
+
+import com.example.backend.model.User;
+import com.example.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserInfoUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userInfo = userRepository.findByName(username);
+        return userInfo.map(UserInfoUserDetails::new)
+                .orElseThrow(()->new UsernameNotFoundException("User not found"+username));
+    }
+}
