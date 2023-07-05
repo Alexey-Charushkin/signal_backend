5ebd90bb18e9f6146fcf22078140d781e92ce7ac
smv <smvexchange@gmail.com>
2023-07-05 17:32:51 +0300

    Add websocket classes

---
+++ backend/src/main/java/com/example/backend/api/GreetingController.java
@@ -0,0 +1,17 @@
+package com.example.backend.api;
+
+import com.example.backend.model.Greeting;
+import com.example.backend.model.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) {
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+}
---
+++ backend/src/main/java/com/example/backend/model/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.backend.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+
+public class Greeting {
+
+    private String content;
+}
