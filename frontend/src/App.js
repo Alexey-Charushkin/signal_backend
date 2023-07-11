40f7d86bdd17800944e42d38d70866f652924b0e
Volbeck <99760572+Volbeck@users.noreply.github.com>
2023-07-11 23:48:49 +0200

    fix frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,108 +1,46 @@
 import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-
-const URL = "ws://websocket-echo.com"
+import  "./stomp"
+const URL = "ws://localhost:8080/new-orders"
 
 const App = () => {
     const [isConnected, setIsConnected] = useState(false);
-    const [name,setName] = useState("");
     const socket = useRef(null);
-    const [response, setResponse] = useState("")
+    const stompClient = useRef(null)
+    const [response, setResponse] = useState([])
     function connect() {
         socket.current = new WebSocket(URL);
-        socket.current.onopen = () => {
-        console.log('WebSocket connection established.');
-        };
-        socket.current.onerror = (error) => {
-            console.error('WebSocket connection error:', error);
-        };
-        socket.current.onclose = () => {
-            console.log('WebSocket connection closed.');
-        }
-        socket.current.onmessage = (event) => {
-            setResponse(event.data)
-        }
+        stompClient.current = Stomp.over(socket.current);
+        stompClient.current.connect({}, function(frame) {
+            console.log('Connected: ' + frame);
+            stompClient.current.subscribe('/topic/order-notifications', function(messageOutput) {
+                console.log(messageOutput)
+                setResponse([...response, messageOutput])
+            });
+        });
         setIsConnected(true)
     }
     function disconnect(){
-        socket.current.close()
+        if(stompClient.current != null) {
+          stompClient.current.disconnect();
+        }
+        console.log("Disconnected");
         setIsConnected(false)
-    }
-    function send(){
-        if (socket.current === null){return}
-        socket.current.send(name)
     }
 
   return (
     <>
-      <noscript>
-        <h2 style={{ color: '#ff0000' }}>
-          Seems your browser doesn't support Javascript! Websocket relies on
-          Javascript being enabled. Please enable Javascript and reload this
-          page!
-        </h2>
-      </noscript>
-      <div id="main-content" className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <form className="form-inline">
-              <div className="form-group">
-                <label htmlFor="connect">WebSocket connection:</label>
-                <button
-                  id="connect"
-                  className="btn btn-default"
-                  type="button"
-                  onClick={connect}
-                  disabled={isConnected}
-                >
-                  Connect
-                </button>
-                <button
-                  id="disconnect"
-                  className="btn btn-default"
-                  type="button"
-                  disabled={!isConnected}
-                  onClick={disconnect}
-                >
-                  Disconnect
-                </button>
-              </div>
-            </form>
+      <div>
+          <div>
+              <button id="connect" onClick={connect} disabled={isConnected}>Connect</button>
+              <button id="disconnect" onClick={disconnect} disabled={!isConnected}>Disconnect</button>
           </div>
-          <div className="col-md-6">
-            <form className="form-inline">
-              <div className="form-group">
-                <label htmlFor="name">What is your name?</label>
-                <input
-                  type="text"
-                  id="name"
-                  className="form-control"
-                  placeholder="Your name here..."
-                  value={name}
-                  onChange={(e)=>{setName(e.target.value)}}
-                />
-              </div>
-              <button id="send" className="btn btn-default" type="button"
-                onClick={send}
-                
-              >
-                Send
-              </button>
-            </form>
+          <br />
+          <div id="conversationDiv">
+            {response.map((item, index) => (
+              <p key={index}>{item}</p>
+            ))}
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            <table id="conversation" className="table table-striped">
-              <thead>
-                <tr>
-                  <th>Greetings {response}</th>
-                </tr>
-              </thead>
-              <tbody id="greetings"></tbody>
-            </table>
-          </div>
-        </div>
       </div>
     </>
   );
