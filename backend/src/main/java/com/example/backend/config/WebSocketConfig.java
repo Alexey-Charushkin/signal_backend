e9859dce8a517c888dd350a925cf8cb864eb6d53
Maksim <smvexchange@gmail.com>
2023-07-12 14:35:01 +0300

    Add javadoc

--- backend/src/main/java/com/example/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/example/backend/config/WebSocketConfig.java
@@ -6,16 +6,29 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+/**
+ * Конфигурационный класс для настройки соединения между клиентом и сервером по протоколу Websocket
+ */
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    /**
+     * Метод для настройки брокера сообщений
+     *
+     * @param config    реестр для настройки брокера сообщений
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic/order-notifications");
         config.setApplicationDestinationPrefixes("/app");
     }
 
+    /**
+     * Метод для добавления ендпоинтов
+     *
+     * @param registry  реестр ендпоинтов для STOMP поверх WebSocket
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/new-orders").setAllowedOrigins("*").withSockJS();
